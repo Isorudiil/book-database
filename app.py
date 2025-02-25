e2e8692ddb37e52312291b5eb800d2da53cba8df
Elijah Herald <elijah.herald@gmail.com>
2025-02-25 14:00:01 -0500

    Worked on main menu functions withing the main app

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from models import (Base, session,
                     Book, engine)
 
+
+def menu():
+    while True:
+        print('''
+            \nPROGRAMMING BOOKS
+            \r1) Add book
+            \r2) View all books
+            \r3) Search for book
+            \r4) Book analysis
+            \r5) Exit''')
+        choice = input('What would you like to do? ')
+
+        if choice in ['1','2', '3', '4', '5']:
+            return choice
+        else:
+            input('''
+                  \rPlease choose one of the options above.
+                  \rA number from 1-5.
+                  \rPress enter to try again''')
+
+
 # import models
 # main menu - add, search, analysis, exit, view
 # add books to the database
@@ -10,6 +31,27 @@ from models import (Base, session,
 # data cleaning
 # loop runs program
 
+def app():
+    app_running = True
+    while app_running:
+        choice = menu()
+        if choice == '1':
+            # add book
+            pass
+        elif choice == '2':
+            # view books
+            pass
+        elif choice == '3':
+            # search books
+            pass
+        elif choice == '4':
+            # amalysis
+            pass
+        else:
+            print('GOODBYE')
+            app_running = False
+
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    app()
